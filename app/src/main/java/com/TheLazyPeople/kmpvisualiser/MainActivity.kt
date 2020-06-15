4962a8ca587371285f99841b53abad982e474b85
legendarygirl56 <ayushidoshi56@gmail.com>
2020-06-15 13:28:17 +0530

    algo added

--- app/src/main/java/com/TheLazyPeople/kmpvisualiser/MainActivity.kt
+++ app/src/main/java/com/TheLazyPeople/kmpvisualiser/MainActivity.kt
@@ -2,6 +2,7 @@ package com.TheLazyPeople.kmpvisualiser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,67 @@ class MainActivity : AppCompatActivity() {
         btnSearch.setOnClickListener {
             sourceString=etSourceString.text.toString()
             targetString=etTargetString.text.toString()
+            KMPSearch(targetString, sourceString)
+        }
+    }
+    fun KMPSearch(pat: String, txt: String) {
+        val M = pat.length
+        val N = txt.length
+
+        // create lps[] that will hold the longest
+        // prefix suffix values for pattern
+        val lps = IntArray(M)
+        var j = 0 // index for pat[]
+
+        // Preprocess the pattern (calculate lps[]
+        // array)
+        computeLPSArray(pat, M, lps)
+        var i = 0 // index for txt[]
+        while (i < N) {
+            if (pat[j] == txt[i]) {
+                j++
+                i++
+            }
+            if (j == M) {
+               // print("Found pattern " + "at index " + (i - j))
+                Toast.makeText(this,"FOUND AT INDEX ${i-j}",Toast.LENGTH_LONG).show()
+                j = lps[j - 1]
+            } else if (i < N && pat[j] != txt[i]) {
+                // Do not match lps[0..lps[j-1]] characters,
+                // they will match anyway
+                if (j != 0) j = lps[j - 1] else i = i + 1
+            }
+        }
+    }
+
+    fun computeLPSArray(pat: String, M: Int, lps: IntArray) {
+        // length of the previous longest prefix suffix
+        var len = 0
+        var i = 1
+        lps[0] = 0 // lps[0] is always 0
+
+        // the loop calculates lps[i] for i = 1 to M-1
+        while (i < M) {
+            if (pat[i] == pat[len]) {
+                len++
+                lps[i] = len
+                i++
+            } else  // (pat[i] != pat[len])
+            {
+                // This is tricky. Consider the example.
+                // AAACAAAA and i = 7. The idea is similar
+                // to search step.
+                if (len != 0) {
+                    len = lps[len - 1]
+
+                    // Also, note that we do not increment
+                    // i here
+                } else  // if (len == 0)
+                {
+                    lps[i] = len
+                    i++
+                }
+            }
         }
     }
 }
